60de7dd94e9a34b306d0985b10a681f10b539d14
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2024-07-14 19:07:42 +0100

    importação do componente global  botão

--- src/pages/create-trip/steps/destination-and-date-step.tsx
+++ src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -1,41 +1,46 @@
 import { ArrowRight, Calendar, MapPin, Settings2 } from "lucide-react";
+import { Button } from "../../../components/button";
 interface DestinationAndDateStepProps {
-    isGuestsInputOpen: boolean;
-    openGuestsInput: () => void;
-    closeGuestsInput: () => void;
+  isGuestsInputOpen: boolean;
+  openGuestsInput: () => void;
+  closeGuestsInput: () => void;
 }
-export function DestinationAndDateStep({ 
-    isGuestsInputOpen, 
-    openGuestsInput, 
-    closeGuestsInput,
+export function DestinationAndDateStep({
+  isGuestsInputOpen,
+  openGuestsInput,
+  closeGuestsInput,
 }: DestinationAndDateStepProps) {
-    return (
+  return (
     <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-        <div className='flex items-center gap-2 flex-1'>
-          <MapPin className='size-5 text-zinc-400' />
-          <input disabled={isGuestsInputOpen} type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
-        </div>
-
-        <div className='flex items-center gap-2'>
-          <Calendar className='size-5 text-zinc-400' />
-          <input disabled={isGuestsInputOpen} type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-53 outline-none" />
-        </div>
-
-        <div className='w-px h-6 bg-zinc-800' />
-
-        {
-          isGuestsInputOpen ? (
-            <button onClick={closeGuestsInput} className='bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700'>
-              Alterar local/data
-              <Settings2 className='size-5' />
-            </button>
-          ) : (
-            <button onClick={openGuestsInput} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
-              Continuar
-              <ArrowRight className='size-5' />
-            </button>
-          )
-        }
+      <div className='flex items-center gap-2 flex-1'>
+        <MapPin className='size-5 text-zinc-400' />
+        <input disabled={isGuestsInputOpen} type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
       </div>
-    );
+
+      <div className='flex items-center gap-2'>
+        <Calendar className='size-5 text-zinc-400' />
+        <input disabled={isGuestsInputOpen} type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-53 outline-none" />
+      </div>
+
+      <div className='w-px h-6 bg-zinc-800' />
+
+      {
+        isGuestsInputOpen ? (
+          <Button 
+            onClick={closeGuestsInput} 
+            variant="secondary" 
+            size="default"
+          >
+          Alterar local/data
+          <Settings2 className='size-5' />
+        </Button>
+        ) : (
+          <Button onClick={openGuestsInput}  variant="primary" size="default">
+            Continuar
+            <ArrowRight className='size-5' />
+          </Button>
+        )
+      }
+    </div>
+  );
 }
